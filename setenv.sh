6ecb532619d468939845eb67408677c14843e8b0
Voodoo3000 <m047wbm@mail.ru>
2019-04-09 17:05:17 +0600

    setenv.sh added
    For tomcat

---
+++ setenv.sh
@@ -0,0 +1,14 @@
+
+
+JAVA_HOME=/usr/java/jdk1.8.0_31
+
+# Below this line we should not hard code any instance specific settings ...
+# So we only have to check/change the first x lines for a new environment.
+
+
+export JAVA_HOME
+export PATH=$PATH:$HOME/bin:$JAVA_HOME/bin
+export CATALINA_HOME
+export CATALINA_BASE
+
+
